2c01ca8b56c7cc41035999b09a1fa63358b1b788
Loan <loan.bouhier@gmail.com>
2024-02-26 17:59:12 +0100

    Added: routes for groups TODO: start front, test these routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authService = require('../services/authService');
 const conversationService = require('../services/conversationService');
+const groupService = require('../services/groupService');
 const messageService = require('../services/messageService');
 const userService = require('../services/userService');
 
@@ -16,6 +17,17 @@ router.get('/conversations', async (req, res) => conversationService.getConversa
 router.get('/conversations/:id', async (req, res) => conversationService.getConversationById(req, res));
 router.get('/conversations/:id/messages', async (req, res) => conversationService.getConversationMessages(req, res));
 
+/* Groups */
+router.post('/groups', async (req, res) => groupService.createGroup(req, res));
+router.get('/groups', async (req, res) => groupService.getGroups(req, res));
+router.get('/groups/:id', async (req, res) => groupService.getGroupById(req, res));
+router.get('/groups/:id/messages', async (req, res) => groupService.getGroupMessages(req, res));
+router.put('/groups/:id', async (req, res) => groupService.renameGroup(req, res));
+router.delete('/groups/:id', async (req, res) => groupService.deleteGroup(req, res));
+router.get('/groups/:id/users', async (req, res) => groupService.getGroupParticipants(req, res));
+router.post('/groups/:id/users/add', async (req, res) => groupService.addParticipant(req, res));
+router.delete('/groups/:id/users/remove', async (req, res) => groupService.removeParticipant(req, res));
+
 /* Messages */
 router.post('/messages', async (req, res) => messageService.createMessage(req, res));
 router.put('/messages/:id', async (req, res) => messageService.updateMessage(req, res));
@@ -24,6 +36,6 @@ router.delete('/messages/:id', async (req, res) => messageService.deleteMessage(
 /* Users */
 router.get('/users', async (req, res) => userService.getAllUsers(req, res));
 router.get('/users/:id', async (req, res) => userService.getUserInfo(req, res));
-router.put('/users/:id', async (req, res) => userService.updateUserInfo(req, res));
+router.put('/users/:id', async (req, res) => userService.updateUser(req, res));
 
 module.exports = router;
--- services/groupService.js
+++ services/groupService.js
@@ -18,25 +18,33 @@ const { v4: uuidv4 } = require('uuid');
 async function createGroup(req, res) {
     /* Check if the request is valid */
     if (!req.body.name || !req.body.userId || !req.body.participants || !req.body.token) {
-        return res.status(400).json({ message: 'Invalid request' });
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
     }
 
     /* Check if the user is authenticated */
     const newToken = await authService.refreshToken(req.body.token, req.body.userId);
     if (!newToken) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
     }
 
     /* Check if the user exists */
     const user = await User.findOne({ id: req.body.userId });
     if (!user) {
-        return res.status(404).json({ message: 'User not found' });
+        return res.status(404).json({
+            message: 'User not found'
+        });
     }
 
     /* Check if the participants exist */
     const participants = await User.find({ id: { $in: req.body.participants } });
     if (participants.length !== req.body.participants.length) {
-        return res.status(404).json({ message: 'Participants not found' });
+        return res.status(404).json({
+            message: 'Participants not found'
+        });
     }
 
     /* Create the group */
@@ -53,12 +61,565 @@ async function createGroup(req, res) {
         /* Remove the _id and __v fields */
         let { _id, __v, ...groupData } = group.toObject();
 
-        return res.status(201).json({ message: 'Group created', group: groupData, token: newToken });
+        return res.status(201).json({
+            message: 'Group created',
+            group: groupData,
+            token: newToken
+        });
     } catch (err) {
-        return res.status(500).json({ message: 'Internal server error' });
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
     }
 }
 
-module.exports = {
+/* Get all the groups of a user
+ * @param {Object} req - The request object
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function getGroups(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.userId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
 
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    const user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found'
+        });
+    }
+
+    /* Get the groups */
+    const groups = await Group.find({ participants: req.body.userId });
+
+    /* Remove the _id and __v fields */
+    groups.map(group => {
+        let { _id, __v, ...groupData } = group.toObject();
+        return groupData;
+    });
+
+    return res.status(200).json({
+        message: 'Groups found',
+        groups: groups,
+        token: newToken
+    });
+}
+
+/* Get a group by its id
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ */
+async function getGroupById(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is part of the group */
+    if (!group.participants.includes(req.body.userId)) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Remove the _id and __v fields */
+    let { _id, __v, ...groupData } = group.toObject();
+
+    return res.status(200).json({
+        message: 'Group found',
+        group: groupData,
+        token: newToken
+    });
+}
+
+
+/* Get the messages of a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function getGroupMessages(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is part of the group */
+    if (!group.participants.includes(req.body.userId)) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Get the messages */
+    let messages = await Message.find({ groupId: req.body.groupId });
+
+    /* Remove the _id and __v fields */
+    messages.map(message => {
+        let { _id, __v, ...messageData } = message.toObject();
+        return messageData;
+    });
+
+    return res.status(200).json({
+        message: 'Messages found',
+        messages: messages,
+        token: newToken
+    });
+}
+
+/* Add a user to a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.participantId - The id of the user to add to the group
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function addParticipant(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.participantId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the participant exists */
+    let participant = await User.findOne({ id: req.body.participantId });
+    if (!participant) {
+        return res.status(404).json({
+            message: 'Participant not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is the owner of the group */
+    if (group.ownerId !== req.body.userId) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Add the participant to the group */
+    group.participants.push(req.body.participantId);
+    try {
+        await group.save();
+
+        /* Remove the _id and __v fields */
+        let { _id, __v, ...groupData } = group.toObject();
+
+        return res.status(200).json({
+            message: 'Participant added',
+            group: groupData,
+            token: newToken
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+}
+
+/* Remove a user from a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.participantId - The id of the user to remove from the group
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function removeParticipant(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.participantId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the participant exists */
+    let participant = await User.findOne({ id: req.body.participantId });
+    if (!participant) {
+        return res.status(404).json({
+            message: 'Participant not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is the owner of the group */
+    if (group.ownerId !== req.body.userId) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Remove the participant from the group */
+    group.participants = group.participants.filter(participant => participant !== req.body.participantId);
+    try {
+        await group.save();
+
+        /* Remove the _id and __v fields */
+        let { _id, __v, ...groupData } = group.toObject();
+
+        return res.status(200).json({
+            message: 'Participant removed',
+            group: groupData,
+            token: newToken
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+}
+
+/* Delete a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function deleteGroup(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is the owner of the group */
+    if (group.ownerId !== req.body.userId) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Delete the group */
+    try {
+        await group.delete();
+
+        /* Delete the messages of the group */
+        await Message.deleteMany({ conversationId: req.body.groupId });
+
+
+        return res.status(200).json({
+            message: 'Group deleted',
+            token: newToken
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+}
+
+/* Rename a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.name - The new name of the group
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function renameGroup(req, res) {
+/* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.name || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({ id: req.body.userId });
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({ id: req.body.groupId });
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is the owner of the group */
+    if (group.ownerId !== req.body.userId) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Rename the group */
+    group.name = req.body.name;
+    try {
+        await group.save();
+
+        /* Remove the _id and __v fields */
+        let { _id, __v, ...groupData } = group.toObject();
+
+        return res.status(200).json({
+            message: 'Group renamed',
+            group: groupData,
+            token: newToken
+        });
+    } catch (err) {
+        return res.status(500).json({
+            message: 'Internal server error'
+        });
+    }
+}
+
+/* Get all the participants of a group
+ * @param {Object} req - The request object
+ * @param {String} req.body.groupId - The id of the group
+ * @param {String} req.body.userId - The id of the user
+ * @param {String} req.body.token - The token of the user
+ * @param {Object} res - The response object
+ * @returns {Object} - The response object
+ */
+async function getGroupParticipants(req, res) {
+    /* Check if the request is valid */
+    if (!req.body.groupId || !req.body.userId || !req.body.token) {
+        return res.status(400).json({
+            message: 'Invalid request'
+        });
+    }
+
+    /* Check if the user is authenticated */
+    const newToken = await authService.refreshToken(req.body.token, req.body.userId);
+    if (!newToken) {
+        return res.status(401).json({
+            message: 'Unauthorized'
+        });
+    }
+
+    /* Check if the user exists */
+    let user = await User.findOne({id: req.body.userId});
+    if (!user) {
+        return res.status(404).json({
+            message: 'User not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the group exists */
+    let group = await Group.findOne({id: req.body.groupId});
+    if (!group) {
+        return res.status(404).json({
+            message: 'Group not found',
+            token: newToken
+        });
+    }
+
+    /* Check if the user is part of the group */
+    if (!group.participants.includes(req.body.userId)) {
+        return res.status(403).json({
+            message: 'Forbidden',
+            token: newToken
+        });
+    }
+
+    /* Get the participants */
+    let participants = await User.find({id: {$in: group.participants}});
+
+    /* Remove the _id and __v fields */
+    participants.map(participant => {
+        let {_id, __v, ...participantData} = participant.toObject();
+        return participantData;
+    });
+
+    return res.status(200).json({
+        message: 'Participants found',
+        participants: participants,
+        token: newToken
+    });
+}
+
+module.exports = {
+    createGroup,
+    getGroups,
+    getGroupById,
+    getGroupMessages,
+    addParticipant,
+    removeParticipant,
+    deleteGroup,
+    renameGroup,
+    getGroupParticipants
 }
