83f8ed00c654fb9617b2d99f1aa59bb377d6a12d
Loan <loan.bouhier@gmail.com>
2024-03-21 19:10:45 +0100

    Added proxy documentation

--- back/ProxyService/server.js
+++ back/ProxyService/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+
 const userRouter = require('./api/routes/userRoutes');
 const conversationRouter = require('./api/routes/conversationRoutes');
 const messageRouter = require('./api/routes/messageRoutes');
@@ -13,6 +16,9 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/conversations', conversationRouter);
 app.use('/messages', messageRouter);
+
+/* Documentation (doc.yaml) */
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(YAML.load('./doc.yaml')));
 
 
 app.listen(process.env.PROXY_SERVICE_PORT, () => console.log(`Service running on port ${process.env.PROXY_SERVICE_PORT}`));
